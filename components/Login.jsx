172bee0138a32a6f46cfcda457099a81dc9b0651
Neha <neharadialcode@gmail.com>
2024-01-25 15:38:07 +0530

    form added

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,7 +1,65 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      {" "}
+      <div className="h-screen flex flex-col justify-center items-center max-w-[700px] mx-auto">
+        <form action="">
+          <input
+            className="border-[1px] border-purple-700 w-full m-1 p-4"
+            type="text"
+            name=""
+            id=""
+            placeholder="First Name"
+          />
+          <input
+            className="border-[1px] border-purple-700 w-full m-1 p-4"
+            type="text"
+            name=""
+            id=""
+            placeholder="Last Name"
+          />
+          <input
+            className="border-[1px] border-purple-700 w-full m-1 p-4"
+            type="text"
+            name=""
+            id=""
+            placeholder="Email"
+          />
+          <input
+            className="border-[1px] border-purple-700 w-full m-1 p-4"
+            type="text"
+            name=""
+            id=""
+            placeholder="Password"
+          />
+          <input
+            className="border-[1px] border-purple-700 w-full m-1 p-4"
+            type="text"
+            name=""
+            id=""
+            placeholder="ConfirmPassword"
+          />
+          <select
+            className="border-[1px] border-purple-700 w-full m-1 p-4"
+            name=""
+            id=""
+          >
+            <option value="">Frontend</option>
+            <option value="">Backend</option>
+            <option value="">UI/UX</option>
+            <option value="">Wordpress</option>
+          </select>
+          <div className="text-center">
+            <button className="bg-purple-700 text-white py-2 px-10 hover:bg-purple-500 transition-all duration-200 ease-in-out rounded-lg mt-4 text-lg">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
